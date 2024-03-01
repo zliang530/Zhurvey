19ff1a162a2203d01febadc9217f9670150ef474
Zhen Liang <zhenqiangliang30@gmail.com>
2024-02-29 21:13:58 -0600

    added basic frontend and backend environment

---
+++ zhurvey/src/main/java/com/zhenql/zhurvey/ZhurveyApplication.java
@@ -0,0 +1,13 @@
+package com.zhenql.zhurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhurveyApplication.class, args);
+	}
+
+}
---
+++ zhurvey/src/test/java/com/zhenql/zhurvey/ZhurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhenql.zhurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
